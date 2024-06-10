04fbfdd7207dc4f3e0d4c3a1d9bde7c742944e3d
hnbae-dev <hnbae.dev@gmail.com>
2024-06-11 00:01:50 +0900

    section 19 - project

--- section17/app.js
+++ section17/app.js
@@ -1,5 +1,5 @@
 // http: 요청, 응답 처리 관련 패키지(객체)
-const http = require("http"); // http 객체 리턴 - 속성, 메서드 사용 가능
+const http = require("http"); // http 객체 리턴 -> 속성, 메서드 사용 가능 / node 내장 객체
 
 function handleRequest(request, response) {
     if (request.url === "/currenttime") {
@@ -12,5 +12,8 @@ function handleRequest(request, response) {
 } // response가 없는 서버는 계속해서 무한으로 요청을 받음 -> time out
 
 const server = http.createServer(handleRequest); // 웹 서버 생성 - 서버 기능 포함된 객체 리턴
-
+// node js로 자체 서버를 생성할 때, 들어오는 요청을 수신할 포트 설정
+// 배포할 때는 80 또는 443으로 변경함
 server.listen(3000); // 포트 번호에서 수신 대기 (포트: 서버 접속을 위한 entrypoint(진입점))
+// amazon.com => 아마존 서버에 요청 전송
+// amazon.com:443 => SSL
